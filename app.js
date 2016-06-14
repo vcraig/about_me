6804fddebb824ab77e79ce3f653b773750caac70
Valerie Craig <valine@gmail.com>
2016-06-14 16:24:52 -0700

    js save

--- app.js
+++ app.js
@@ -2,5 +2,15 @@
 var visitorName = prompt('Would you kindly input your name?');
 alert('Howdy ' + visitorName + '!');
 
+var answer1 = prompt('Q1: Does Val care about astrology?').toLowerCase();
+if (answer1 === 'yes' || answer1 === 'y') {
+
+  alert('Correct!');
+}
+else {
+  alert('Wrong!');
+
+}
+
 
 //answer1.toLowerCase();
